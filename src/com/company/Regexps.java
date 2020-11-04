1bf3a78a23f3b585a556a988cc92f8268d221884
Kirill <kirill.brezhnev02@gmail.com>
2020-11-04 21:11:59 +0200

    ssss ssss

--- src/com/company/Regexps.java
+++ src/com/company/Regexps.java
@@ -8,21 +8,25 @@ public class Regexps {
         Matcher m = p.matcher(S);
         return m.matches();
     }
+
     public boolean RegNumber(String S) {
         Pattern p = Pattern.compile("[A-Za-z0-9]{1,10}");
         Matcher m = p.matcher(S);
         return m.matches();
     }
+
     public boolean RegBase(String S) {
         Pattern p = Pattern.compile("[0-9]+");
         Matcher m = p.matcher(S);
         return m.matches();
     }
+
     public boolean RegComplex(String S) {
         Pattern p = Pattern.compile("-?\\d*\\.?\\d+");
         Matcher m = p.matcher(S);
         return m.matches();
     }
+
     public boolean RegEquation(String S) {
         Pattern p = Pattern.compile("-?[0-9]{1,10}");
         Matcher m = p.matcher(S);
